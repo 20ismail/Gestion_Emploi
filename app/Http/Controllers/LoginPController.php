5ba1728056c13f90773c66ee06549e3d886b816d
mohammed harrous <m.harrous9240@uca.ac.ma>
2024-05-23 16:20:15 +0200

    Update LoginPController.php

--- app/Http/Controllers/LoginPController.php
+++ app/Http/Controllers/LoginPController.php
@@ -1,29 +1,27 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Professeur;
 use Illuminate\Support\Facades\Hash;
-
 class LoginPController extends Controller
 {
     public function authenticate(Request $request)
     {
-         
-        $credentials = $request->validate([
+        $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
+       
+        $prof = Professeur::where('email', $request->email)->first();
 
-        if (Auth::attempt($credentials)) {
+        if ($prof && Hash::check($request->password, $prof->password)) {
+            Auth::login($prof);
             $request->session()->regenerate();
             return redirect()->intended('home');
         }
 
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput('email');
+        ]);
     }
 }
